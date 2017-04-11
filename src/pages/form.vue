12f3ea32ea22c638df14559e60594f90b8b71076
chris <419608324@qq.com>
2017-04-11 19:50:12 +0800

    han

--- src/pages/form.vue
+++ src/pages/form.vue
@@ -1,145 +1,111 @@
 <template>
   <f7-page>
     <f7-navbar title="Bug Info" back-link="Back" sliding>
-    <f7-nav-right>
-      <f7-link   @click="updateBug()" >update</f7-link>
-    </f7-nav-right>
+      <f7-nav-right>
+        <f7-link   @click="updateBug()" >update</f7-link>
+      </f7-nav-right>
     </f7-navbar>
-    <f7-block-title>{{ subject }} </f7-block-title>
+    <f7-block-title></f7-block-title>
     <f7-list form>
       <f7-list-item>
-        <f7-label>bugId</f7-label>
-        <f7-input type="text" placeholder="Name">{{ bugInfo.bugNo }}</f7-input>
+        <f7-label>Bug No</f7-label>
+        <f7-input type="text" placeholder="Name" :value="bugInfo.bugNo"></f7-input>
+      </f7-list-item>
+
+      <f7-list-item>
+        <f7-label >Filed By:</f7-label>
+        <f7-input type="text" >{{bugInfo.filedByName}}</f7-input>
       </f7-list-item>
       <f7-list-item>
-        <f7-label>Filed</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
+        <f7-label>Filed:</f7-label>
+        <f7-input type="text" >{{bugInfo.filed}}</f7-input>
       </f7-list-item>
       <f7-list-item>
-        <f7-label>Assigned</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
+        <f7-label>Assigned:</f7-label>
+        <f7-input type="text" >{{bugInfo.assignedName}}</f7-input>
       </f7-list-item>
       <f7-list-item>
-        <f7-label>Mail</f7-label>
-        <f7-input type="email" placeholder="E-mail"></f7-input>
+        <f7-label>Customer:</f7-label>
+        <f7-input type="text" >{{bugInfo.customer}}</f7-input>
       </f7-list-item>
       <f7-list-item>
-        <f7-label>Custom</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
+        <f7-label>BugType:</f7-label>
+        <f7-input type="text" >{{bugInfo.bugType}}</f7-input>
       </f7-list-item>
       <f7-list-item>
-        <f7-label>Status</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
+        <f7-label>Status:</f7-label>
+        <f7-input type="text">{{bugInfo.statusIdName}}</f7-input>
       </f7-list-item>
       <f7-list-item>
-        <f7-label>Fixed by</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
+        <f7-label>Fixed Ver:</f7-label>
+        <f7-input type="text">{{bugInfo.fixedVer}}</f7-input>
       </f7-list-item>
       <f7-list-item>
-        <f7-label>Introduced in</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Component</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Component</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
+        <f7-label>Subject</f7-label>
+        <f7-input type="text" >{{bugInfo.subject}}</f7-input>
       </f7-list-item>
       <f7-list-item>
         <f7-label>Component</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
+        <f7-input type="text" >{{bugInfo.componentIdName}}</f7-input>
       </f7-list-item>
       <f7-list-item>
-        <f7-label>Component</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
+
       </f7-list-item>
-      <f7-list-item>
-        <f7-label>Component</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
+      <f7-list-item v-for="n in updates"
+                    :key="n"
+                    :title="'' + n.time"
+                    :subtitle="''"
+                    :data="n"
+
+      >
+
+        <f7-label>{{n.chg}}</f7-label>
       </f7-list-item>
-      <f7-list-item>
-        <f7-label>Component</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Component</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>URL</f7-label>
-        <f7-input type="url" placeholder="URL"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Phone</f7-label>
-        <f7-input type="tel" placeholder="Phone"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Birth date</f7-label>
-        <f7-input type="date" placeholder="Birth date" value="2014-04-30"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Date time</f7-label>
-        <f7-input type="datetime-local"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Gender</f7-label>
-        <f7-input type="select">
-          <option value="1">Male</option>
-          <option value="1">Female</option>
-        </f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Textarea</f7-label>
-        <f7-input type="textarea" placeholder="Textarea"></f7-input>
-      </f7-list-item>
+
     </f7-list>
 
-    <f7-block-title>Updates</f7-block-title>
-    <f7-list form>
-      <f7-list-item v-for="n in 3" checkbox name="my-checkbox" :value="n" :title="'Checkbox ' + n"></f7-list-item>
-    </f7-list>
 
-    <f7-block-title>Checkboxes</f7-block-title>
-    <f7-list form>
-      <f7-list-item v-for="n in 3" checkbox name="my-checkbox" :value="n" :title="'Checkbox ' + n"></f7-list-item>
-    </f7-list>
-
-    <f7-block-title>Radios</f7-block-title>
-    <f7-list form>
-      <f7-list-item v-for="n in 3" radio name="my-radio" :checked="n === 1" :value="n" :title="'Radio ' + n"></f7-list-item>
-    </f7-list>
   </f7-page>
 </template>
 
 <script>
-export default {
-    data: function(){
-        return {
-           bugInfo:""
-        }
-    },
-    computed: {
-        subject: function(){
-           return  this.bugInfo.bugNo+ " " +this.bugInfo.subject;
-        }
-    },
-    mounted(){
-      let query = this.$route.query;
-      let bugId = query["bugId"];
-      this.$http({url: "findByBugNo", params:{bugNo: bugId}, method: 'GET'}).then((response) =>
-      {
-         this.bugInfo = response.data;
-      },(response) => {
-          console.log("bugInfo null");
-      });
-    },
-    methods:{
-        updateBug: function(){
-           let urlpath = "/update/?bugId="+ this.bugInfo.bugNo;
-           this.$f7.mainView.router.load({url: urlpath})
+    export default {
+        data: function(){
+            return {
+                bugInfo:"",
+                user:"",
+                updates:[]
+            }
+        },
+        computed: {
+            subject: function(){
+                return  this.bugInfo.bugNo+ " " +this.bugInfo.subject;
+            }
+        },
+        mounted(){
+            let query = this.$route.query;
+            let bugId = query["bugId"];
+            this.$http({url: "findByBugNo", params:{bugNo: bugId}, method: 'GET'}).then((response) =>
+            {
+                this.bugInfo = response.data;
+            },(response) => {
+                console.log("bugInfo null");
+            });
+            console.log(bugId)
+            let url1="findUpdatesByBugNo/?bugNo="+bugId;
+            this.$http({url: url1, method: 'GET'}).then((response) => {
+                this.updates = response.data;
+                console.log(data);
+            }, (response) => {
+                console.log("invalid username or password");
+            });
+        },
+
+        methods:{
+            updateBug: function(){
+                let urlpath = "/update/?bugId="+ this.bugInfo.bugNo;
+                this.$f7.mainView.router.load({url: urlpath})
+            }
         }
     }
-}
 </script>
--- src/pages/update-form.vue
+++ src/pages/update-form.vue
@@ -1,144 +1,104 @@
 <template>
   <f7-page>
     <f7-navbar title="Bug Info" back-link="Back" sliding>
-    <f7-nav-right>
-      <f7-link icon-size="22">save</f7-link>
-    </f7-nav-right>
+      <f7-nav-right>
+        <f7-link icon-size="22"  @click="saveBug()">save</f7-link>
+      </f7-nav-right>
     </f7-navbar>
     <f7-block-title> {{ subject }} </f7-block-title>
     <f7-list form>
       <f7-list-item>
-        <f7-label>bugId</f7-label>
-        <f7-input type="text" placeholder="Name">{{ bugInfo.bugNo }}</f7-input>
+        <f7-label>Bug No:</f7-label>
+        <f7-input type="text" placeholder="Name" >{{bugInfo.bugNo}}</f7-input>
+      </f7-list-item>
+
+      <f7-list-item>
+        <f7-label>Filed By:</f7-label>
+        <f7-input type="text" bugInfo.filedByName>{{bugInfo.filedByName}}</f7-input>
       </f7-list-item>
       <f7-list-item>
-        <f7-label>Filed</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
+        <f7-label>Assigned:</f7-label>
+        <f7-input type="text" :value="bugInfo.assignedName"></f7-input>
+      </f7-list-item>
+      <f7-list-item smart-select>
+        <f7-label>Product:</f7-label>
+        <!-- Select with values inside -->
+        <select name="status" :value="bugInfo.productIdName">{{}}
+          <option
+                  v-for="p in product"
+                  :data="p"
+                  :value="p.id"
+                  selected>{{p.description}}</option>
+
+        </select>
+      </f7-list-item>
+      <f7-list-item smart-select>
+        <f7-label>Status:</f7-label>
+        <!-- Select with values inside -->
+        <select name="status" :value="bugInfo.productIdName">
+          <option
+                  v-for="s in status"
+                  :data="s"
+                  :value="s.id"
+                   >{{s.description}}</option>
+        </select>
       </f7-list-item>
       <f7-list-item>
-        <f7-label>Assigned</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
+        <f7-label>Subject:</f7-label>
+        <f7-input type="text" placeholder>{{bugInfo.subject}}</f7-input>
       </f7-list-item>
       <f7-list-item>
-        <f7-label>Mail</f7-label>
-        <f7-input type="email" placeholder="E-mail"></f7-input>
+        <f7-label>Comment:</f7-label>
+        <f7-input type="textarea" placeholder></f7-input>
       </f7-list-item>
-      <f7-list-item>
-        <f7-label>Custom</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Status</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Fixed by</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Introduced in</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Component</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Component</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Component</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Component</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Component</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Component</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Component</f7-label>
-        <f7-input type="text" placeholder="demo"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>URL</f7-label>
-        <f7-input type="url" placeholder="URL"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Phone</f7-label>
-        <f7-input type="tel" placeholder="Phone"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Birth date</f7-label>
-        <f7-input type="date" placeholder="Birth date" value="2014-04-30"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Date time</f7-label>
-        <f7-input type="datetime-local"></f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Gender</f7-label>
-        <f7-input type="select">
-          <option value="1">Male</option>
-          <option value="1">Female</option>
-        </f7-input>
-      </f7-list-item>
-      <f7-list-item>
-        <f7-label>Textarea</f7-label>
-        <f7-input type="textarea" placeholder="Textarea"></f7-input>
-      </f7-list-item>
+
     </f7-list>
 
-    <f7-block-title>Updates</f7-block-title>
-    <f7-list form>
-      <f7-list-item v-for="n in 3" checkbox name="my-checkbox" :value="n" :title="'Checkbox ' + n"></f7-list-item>
-    </f7-list>
-
-    <f7-block-title>Checkboxes</f7-block-title>
-    <f7-list form>
-      <f7-list-item v-for="n in 3" checkbox name="my-checkbox" :value="n" :title="'Checkbox ' + n"></f7-list-item>
-    </f7-list>
-
-    <f7-block-title>Radios</f7-block-title>
-    <f7-list form>
-      <f7-list-item v-for="n in 3" radio name="my-radio" :checked="n === 1" :value="n" :title="'Radio ' + n"></f7-list-item>
-    </f7-list>
   </f7-page>
 </template>
 
 <script>
-export default {
-      data: function(){
-          return {
-             bugInfo:""
-          }
-      },
-      computed: {
-          subject: function(){
-             return  this.bugInfo.bugNo+ " " +this.bugInfo.subject;
-          }
-      },
-      mounted(){
-        let query = this.$route.query;
-        let bugId = query["bugId"];
-        this.$http({url: "findByBugNo", params:{bugNo: bugId}, method: 'GET'}).then((response) =>
-        {
-           this.bugInfo = response.data;
-        },(response) => {
-            console.log("bugInfo null");
-        });
-      },
-      methods:{
-          saveBug: function(){
+    export default {
+        data: function(){
+            return {
+                bugInfo:"",
+                status:[],
+                product:[]
+            }
+        },
+        computed: {
+            subject: function(){
+                return  this.bugInfo.bugNo+ " " +this.bugInfo.subject;
+            }
+        },
+        mounted(){
+            let query = this.$route.query;
+            let bugId = query["bugId"];
+            this.$http({url: "findByBugNo", params:{bugNo: bugId}, method: 'GET'}).then((response) =>
+            {
+                this.bugInfo = response.data;
+            },(response) => {
+                console.log("bugInfo null");
+            });
 
-          }
-      }
-}
+            this.$http({url: "findAllStatus", method: 'GET'}).then((response) =>
+            {
+                this.status = response.data;
+            },(response) => {
+                console.log("bugInfo null");
+            });
+
+            this.$http({url: "findAllProduct", method: 'GET'}).then((response) =>
+            {
+                this.product = response.data;
+            },(response) => {
+                console.log("bugInfo null");
+            });
+        },
+        methods:{
+            saveBug: function(){
+
+            }
+        }
+    }
 </script>
